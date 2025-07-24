0460181792144a4fc02eb476e91189130a2f81fc
tosil <Jooooooooooooooooo@users.noreply.github.com>
2025-07-24 11:00:23 +0900

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("MCS.PrintBoard")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MCS.PrintBoard")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("872f1fa1-989d-4075-94f3-1568ae08dc27")]
+[assembly: AssemblyFileVersion("19.10.21.1")]
+[assembly: AssemblyVersion("19.10.21.1")]
